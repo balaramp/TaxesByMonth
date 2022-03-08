749fcc4c5a86b8018d2c7a6543b4127c4a32d390
bpalasamudrum <30477142+bpalasamudrum@users.noreply.github.com>
2022-03-08 16:01:42 -0500

    Add project files.

---
+++ TaxesByMOnth/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+TaxesByMonth.TaxesByMonth taxesByMonth = new TaxesByMonth.TaxesByMonth();
+var taxes = taxesByMonth.CalculateTaxesByMonth();
+
+foreach (TaxesByMonth.TaxesByMonth.TaxesByMonthL i in taxes)
+{
+    string month = "";
+    switch (i.NumberMonth)
+    {
+        case (1):
+            month = "January";
+            break;
+        case (2):
+            month = "February";
+            break;
+        case (3):
+            month = "March";
+            break;
+        case (4):
+            month = "April";
+            break;
+        case (5):
+            month = "May";
+            break;
+        case (6):
+            month = "June";
+            break;
+        case (7):
+            month = "July";
+            break;
+        case (8):
+            month = "August";
+            break;
+        case (9):
+            month = "Sepember";
+            break;
+        case (10):
+            month = "October";
+            break;
+        case (11):
+            month = "November";
+            break;
+        case (12):
+            month = "December";
+            break;
+    }
+    Console.WriteLine(month + " Taxes = $" + i.Taxes);
+}
+
+Console.ReadLine();
---
+++ TaxesByMOnth/TaxesByMonth.cs
@@ -0,0 +1,37 @@
+﻿using CustomersInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxesByMonth
+{
+    internal class TaxesByMonth
+    {
+        CustomersInfo.CustomerInfo customersInfo;
+        public TaxesByMonth()
+        {
+            customersInfo = new CustomersInfo.CustomerInfo();
+            customersInfo.Init();
+        }
+
+        public List<TaxesByMonthL> CalculateTaxesByMonth()
+        {
+            return customersInfo.GetCustomers().GroupBy(x => x.Value.Day.Month).Select(x => new TaxesByMonthL
+            {
+                NumberMonth = x.Key,
+                TotalSales = x.Sum(a => a.Value.Sale),
+                Taxes = x.Sum(a => a.Value.Sale) * 0.08625
+            }).OrderBy(x => x.NumberMonth)
+            .ToList();
+        }
+
+        public class TaxesByMonthL
+        {
+            public int NumberMonth;
+            public int TotalSales;
+            public double Taxes;
+        }
+    }
+}
